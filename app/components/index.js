f046763958fe6027b7a5f6b5b2cb679cdf26537d
shanshanfei <shanshanfei@users.noreply.github.com>
2016-11-01 09:57:27 +0800

    更新注释

--- app/components/index.js
+++ app/components/index.js
@@ -6,6 +6,6 @@ import './World/index.less';
 import Hello from './Hello/index.js';
 import World from './World/index.js';
 
-//nodejs require，与import作用相同
+//ES5语法require，与import作用相同
 // require('./Hello/index.js');
 // require('./World/index.js');
